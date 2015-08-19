f4bc8394ab4d9d018d6d9c730db2c4d57bf381e0
spudlig <wieder.oliver@gmail.com>
2015-08-19 23:18:06 +0200

    no shift yet

--- pack.R
+++ pack.R
@@ -6,41 +6,130 @@ boot_wei <- function(x,i) {
   y0.wei<-quawei(f=0.99, para=pelwei(samlmu(x[i]),bound=0))
 }
 
+setwd("~/Studium/Watermanagement/Masterarbeit/data/data/")
 
-#######
-dat_seq<-rep((seq(c(ISOdate(1948,1,1)), by = "day", length.out = c(60*365.25+1))),227)
-dat_seq_max<-rep(seq(1948,1977,1),227)
 
-listko_df<-matrix(NA,227*60*365.25+227,2)
-maxOL_df<-matrix(NA,227*60,2)
-shiftst_df<-matrix(NA,227*30,5)
+#### function for one input
+# file name needs to be put in within ""
+# window_years -> in years, start_date : as "1903-01-01", then in the function there is a ;--:--; added
+# set wd where the file is located (setwd(""))
+# HQ 30,50,80,100,300,500 possible
+AMS<-function(df,...){
+  nSubsets<-NROW(df)/365.25
+  outList<-vector("list",length=nSubsets)
+  maxOL<-vector("numeric",length=nSubsets)
+  totRow<-nrow(df)
+  
+  for (i in seq_len(nSubsets)){
+    rowsToGrab<-seq(i*365.25-365.25,(365.25*i),1)
+    outList[i]<-df[rowsToGrab]
+    maxOL[i]<-max(outList[[i]],na.rm=T)
+  } 
+  return(maxOL)
+}
 
-for(j in seq(1:227)){
-  listko_df[c((1+j*60*365.25-60*365.25):(j*60*365.25)),1]<-listko[[j]][,4][1:(60*365.25)]
-  listko_df[c((1+j*60*365.25-60*365.25):(j*60*365.25)),2]<-rep(names(listko[j]),(60*365.25))
+shift_it<-function(df,quantile,yrs_avail,yrs_cal,...){  
+  nSubsets<-(length(df)/365.25)
+  outList<-vector("list",length=nSubsets)
+  maxOL<-vector("numeric",length=nSubsets)
+  
+  for (i in seq_len(nSubsets)){
+    rowsToGrab<-seq(i*365.25-365.25,(365.25*i),1)
+    outList[[i]]<-df[rowsToGrab]
+    maxOL[[i]]<-max(outList[[i]],na.rm=T)
+  } 
+  nyrs<-yrs_avail-yrs_cal-1
+  shift<-matrix(NA,nyrs,4)
+  colnames(shift)<-c("Wei","Gum","PE3","LN3")
+  
+  for (i in seq_along(shift[,1])){
+    
+    seqq<-seq(i,yrs_cal+i,1)
+    y0.wei<-quawei(f=quantile, para=pelwei(samlmu(maxOL[seqq]),bound=0))
+    y0.gum<-quagum(f=quantile, para=pelgum(samlmu(maxOL[seqq])))
+    y0.pe3<-quape3(f=quantile, para=pelpe3(samlmu(maxOL[seqq])))
+    y0.ln3<-qualn3(f=quantile, para=pelln3(samlmu(maxOL[seqq]),bound=0))
+    shift[i,1]<-y0.wei
+    shift[i,2]<-y0.gum
+    shift[i,3]<-y0.pe3
+    shift[i,4]<-y0.ln3
+    
+  }
+  return(shift)
 }
 
 
-for(j in seq(1:227)){
-  maxOL_df[c((1+j*60-60):(j*60)),1]<-maxOL_list[[j]]
-  maxOL_df[c((1+j*60-60):(j*60)),2]<-rep(names(maxOL_list[j]),(60))
+readt<-function(file_name,window_years,start_date,HQ,...){
+  window_n<-window_years*365.25
+  path<-paste("./",file_name,sep="")
+  tab<-read.table(path,sep=";",dec=".",skip=1,header=T)
+  tab<-tab[,-c(2,4,5)]
+  tab[,2]<-as.numeric(tab[,2])
+  years_start<-as.character(tab[1,1])
+  years_end<-as.character(tail(tabl1[,1]))[6]
+  where_NA<-tab[grep("-999", tab[,2]),]
+  wd_start<-tab[grep(start_date, tab[,1]),]
+  seq_wd<-c(as.numeric(rownames(wd_start)):(as.numeric(window_n)+as.numeric(rownames(wd_start))))
+  if (min(tab[,2])<(-1)){
+    warning("There are missing values in the selected time window, please select another data file or window ---
+    Look into the output (second list) in order to see the year(s) of the missing value(s), maybe
+            your selected time window doesn't fall in")
+  } 
+  else{
+    print(paste("Time Series Begin:",years_start," - Time Series End:",years_end,
+                " - Your window:",sep=""))
+  }
+  
+  if(HQ==30){
+  quan<-(1-1/30)  
+  } else{
+    if(HQ==50){
+      quan<-(1-1/50)  
+    }
+  else{
+    if(HQ==80){
+      quan<-(1-1/80)  
+    }
+  else{
+    if(HQ==100){
+      quan<-(1-1/100)  
+    }
+  else{
+    if(HQ==150){
+      quan<-(1-1/150)  
+    }
+  else{
+    if(HQ==200){
+      quan<-(1-1/200)  
+    }
+  else{
+    if(HQ==300){
+      quan<-(1-1/300)  
+    }
+  }}}}}}
+  maxOL<-as.matrix(AMS(tab[,2][seq_wd]))
+  shift<-matrix(NA,1,4)
+  colnames(shift)<-c("Wei","Gum","PE3","LN3")
+    y0.wei<-quawei(f=quan, para=pelwei(samlmu(maxOL),bound=0))
+    y0.gum<-quagum(f=quan, para=pelgum(samlmu(maxOL)))
+    y0.pe3<-quape3(f=quan, para=pelpe3(samlmu(maxOL)))
+    y0.ln3<-qualn3(f=quan, para=pelln3(samlmu(maxOL),bound=0))
+    shift[1,1]<-y0.wei
+    shift[1,2]<-y0.gum
+    shift[1,3]<-y0.pe3
+    shift[1,4]<-y0.ln3
+    
+  listt<-list(tab,where_NA,maxOL,shift)
+  names(listt)<-c("Raw Data","NAs Dates","AMS",paste("HQ: ",HQ,sep=""))
+    return(listt)
+} 
+
+#"6128101.day" - no -999
+tabl<-readt("6128101.day",window_years=30,start_date="1970-01-01",HQ=100)
+
+tabl$AMS
+else{
+  if(HQ != c(30,50,80,100,150,200,300){
+    print("The chosen return level is not supported; Choose either '30,50,80,100,150,200 or 300'")
+  }
 }
-
-for(j in seq(1:227)){
-  shiftst_df[c((1+j*30-30):(j*30)),1]<-shiftst[[j]][,1]
-  shiftst_df[c((1+j*30-30):(j*30)),2]<-shiftst[[j]][,2]
-  shiftst_df[c((1+j*30-30):(j*30)),3]<-shiftst[[j]][,3]
-  shiftst_df[c((1+j*30-30):(j*30)),4]<-shiftst[[j]][,4]
-  shiftst_df[c((1+j*30-30):(j*30)),5]<-rep(names(shiftst[j]),(30))
-}
-
-listko_df<-data.frame(listko_df,listko_df,dat_seq)
-maxOL_dff<-data.frame(maxOL_df[,1],maxOL_df[,2],dat_seq_max)
-shiftst_dff<-data.frame(shiftst_df,dat_seq_max)
-
-
-#xyplot(listko_dff[,1]~listko_dff[,3]|listko_dff[,2],type="smooth")
-#xyplot(maxOL_dff[,1]~maxOL_dff[,3]|maxOL_dff[,2],type=c("l","smooth"))
-xyplot(shiftst_dff[,1]~shiftst_dff[,6]|shiftst_dff[,5],type=c("l","smooth"))
-
-
